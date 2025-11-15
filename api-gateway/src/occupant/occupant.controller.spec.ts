b30d23bcebc6f82e9cc48da6f23a973e0e3d8dfd
OMGBA FOUDA <omgba.joseph@institutsaintjean.org>
2025-11-15 18:29:00 +0100

    fix(ci): add nest js import

--- api-gateway/src/occupant/occupant.controller.spec.ts
+++ api-gateway/src/occupant/occupant.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OccupantsController } from './occupant.controller';
+import { HttpModule } from '@nestjs/axios'; // <-- importer HttpModule
 
 describe('OccupantController', () => {
   let controller: OccupantsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule], // <-- ajouter ici
       controllers: [OccupantsController],
     }).compile();
 
