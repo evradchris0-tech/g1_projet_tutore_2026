1a43207aa0ccc50df0a720987a3474c815f927a2
davidloic10 <davidloic10@gmail.com>
2025-11-19 17:06:34 +0100

    fix(cors):résolution du problème de cors

--- src/api/http.js
+++ src/api/http.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://immo360-auth-service.onrender.com',
+  // baseURL: 'https://immo360-auth-service.onrender.com',
+  baseURL: 'http://localhost:3000',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
-//   timeout: 10000,
-//   headers: {
-//     'Content-Type': 'application/json',
-        'Authorization': 'Bearer your_token_here',
-//   },
 });
 
 // Ajouter un intercepteur pour inclure le token d'authentification dans les en-têtes
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
