63c821c30fc7f9aa41ce8787dda13820ebff2900
OMGBA FOUDA <omgba.joseph@institutsaintjean.org>
2025-11-21 11:06:31 +0100

    Configure CI/CD pipeline for GitLab Pages

--- src/api/http.js
+++ src/api/http.js
@@ -1,25 +1,56 @@
+// import axios from 'axios';
+
+// export const api = axios.create({
+//   baseURL: '/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// // Ajouter un intercepteur pour inclure le token d'authentification dans les en-têtes
+// // api.interceptors.request.use(
+// //   (config) => {
+// //     const token = localStorage.getItem('token');
+// //     if (token) {
+// //       config.headers.Authorization = `Bearer ${token}`;
+// //     }
+// //     return config;
+// //   },
+// //   (error) => {
+// //     return Promise.reject(error);
+// //   }
+// // );
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   const publicEndpoints = ['/auth/register', '/auth/login'];
+//   const isPublicEndpoint = publicEndpoints.some(endpoint => config.url === endpoint);
+  
+//   if (token && !isPublicEndpoint) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// // export const authAPI = {
+// //   register: (userData) => api.post('/auth/register', userData),
+// //   login: (credentials) => api.post('/auth/login', credentials),
+// //   // getProfile: () => api.get('/auth/profile'),
+// // };
+
+
+
+// // export default api;
+
+
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-// Ajouter un intercepteur pour inclure le token d'authentification dans les en-têtes
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
@@ -31,13 +62,3 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-// export const authAPI = {
-//   register: (userData) => api.post('/auth/register', userData),
-//   login: (credentials) => api.post('/auth/login', credentials),
-//   // getProfile: () => api.get('/auth/profile'),
-// };
-
-
-
-// export default api;
