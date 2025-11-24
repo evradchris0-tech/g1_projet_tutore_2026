36b028a7438bf09dae88aee464170f125041a34c
davidloic10 <davidloic10@gmail.com>
2025-11-24 11:15:37 +0100

    fix:correction connexion

--- src/api/http.js
+++ src/api/http.js
@@ -1,53 +1,7 @@
-// import axios from 'axios';
-
-// export const api = axios.create({
-//   baseURL: '/api',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// // Ajouter un intercepteur pour inclure le token d'authentification dans les en-têtes
-// // api.interceptors.request.use(
-// //   (config) => {
-// //     const token = localStorage.getItem('token');
-// //     if (token) {
-// //       config.headers.Authorization = `Bearer ${token}`;
-// //     }
-// //     return config;
-// //   },
-// //   (error) => {
-// //     return Promise.reject(error);
-// //   }
-// // );
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   const publicEndpoints = ['/auth/register', '/auth/login'];
-//   const isPublicEndpoint = publicEndpoints.some(endpoint => config.url === endpoint);
-
-//   if (token && !isPublicEndpoint) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// // export const authAPI = {
-// //   register: (userData) => api.post('/auth/register', userData),
-// //   login: (credentials) => api.post('/auth/login', credentials),
-// //   // getProfile: () => api.get('/auth/profile'),
-// // };
-
-// // export default api;
-
 import axios from 'axios'
 
 export const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://immo360-auth-service.onrender.com',
-=======
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://immo360-auth-service.onrender.com',
->>>>>>> 97a3049bb2eada32451b50c58738a847a56db0f9
+  baseURL: 'https://immo360-api-gateway.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -55,7 +9,7 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
-  const publicEndpoints = ['/auth/register', '/auth/login']
+  const publicEndpoints = ['/auth/login', '/auth/register']
   const isPublicEndpoint = publicEndpoints.some((endpoint) => config.url === endpoint)
 
   if (token && !isPublicEndpoint) {
