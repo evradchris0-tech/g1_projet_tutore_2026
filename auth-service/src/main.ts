441057bb1e3c8edc9490d9796dc85fef65d0373d
davidloic10 <davidloic10@gmail.com>
2025-11-25 10:49:34 +0100

    fix:modif des fichiers tsconfig.json et main.ts de authservice pour résoudre le problème de CORS

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -4,13 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // ignore unknown props
-      forbidNonWhitelisted: true, // reject unexpected props
-      transform: true, // auto-transform JSON into DTO classes
-    }),
-  );
+  app.enableCors(
+  //   {
+  //   origin: true,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // }
+);
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true, // ignore unknown props
+  //     forbidNonWhitelisted: true, // reject unexpected props
+  //     transform: true, // auto-transform JSON into DTO classes
+  //   }),
+  // );
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
